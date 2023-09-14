cfb8689f99e4d7719e0c2fbb3b8bbe1623d7e5e5
Bryan <bryan.ferrando@epitech.eu>
2023-09-14 13:55:26 +0100

    Fix tab

--- challenges.py
+++ challenges.py
@@ -17,17 +17,16 @@ print(f"Caesar crypt: {c}")
 print(f"Caesar decrypt: {d}")
 
 def vigenere(string: str, key: str, decrypt: bool = False) -> str:
-    t = ""
-    for i in range(len(string)):
-      c = string[i]
-      k = ord(key[i%len(key)]) * (-1 if decrypt else 1)
-      a = ord('A' if c.isupper() else 'a')
-      if(c.isalpha()):
-        t += chr((ord(c) - a + k) % 26 + a)
-      else:
-        t += c
-    return t
-        
+  t = ""
+  for i in range(len(string)):
+    c = string[i]
+    k = ord(key[i%len(key)]) * (-1 if decrypt else 1)
+    a = ord('A' if c.isupper() else 'a')
+    if(c.isalpha()):
+      t += chr((ord(c) - a + k) % 26 + a)
+    else:
+      t += c
+  return t
 
 v1 = input("Vigenere text: ")
 v2 = input("Vigenere key: ")

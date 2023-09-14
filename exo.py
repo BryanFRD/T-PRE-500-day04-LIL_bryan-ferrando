9e099028230679f8ceebed125a63881ede0b7de4
Bryan <bryan.ferrando@epitech.eu>
2023-09-14 09:38:14 +0100

    First commit

---
+++ exo.py
@@ -0,0 +1,79 @@
+ipt = int(input("Number: "))
+print(f"Is equal to 42: {ipt == 42}")
+print("This integer is " + "even" if ipt % 2 == 0 else "odd")
+
+ipt2 = input("Password: ")
+print("access granted" if ipt2 == "open sesame" else
+       "access fucking granted" if ipt2 == "will you open, you goddamn !¤*@¡" else "permission denied")
+
+ipt3 = int(input("Number: "))
+if ipt3 == 42 or ipt <= 21 or ipt3%2 == 0 or ipt3/2 < 21 or (ipt3 % 2 == 1 and ipt3 >= 45):
+  print("OK")
+else:
+  print("You got wrong my poor friend")
+
+a = 42
+b = 41
+if a == b:
+  print("A and B are the same")
+elif b <= a:
+  print("B is equal or lower than A")
+elif b != a:
+  print("B is different from A")
+
+for i in range(1, 10001):
+  print(i)
+
+ipt4 = input("Input: ")
+tmp = ""
+for c in ipt4:
+  tmp+= c+c
+print(tmp)
+
+for i in range(10001, 1, -1):
+  if(i%7 == 0):
+    print(i)
+
+for i in range(-30, 30):
+  if i%3 == 0 and i%5 == 0:
+    print("FizzBuzz")
+  elif i%3 == 0:
+    print("Fizz")
+  elif i%5 == 0:
+    print("Buzz")
+  else:
+    print(i)
+    
+for i in range(99, 1, -1):
+  print(f"{i} bottles of age appropriate beverages on the wall")
+
+ipt5 = int(input("Number: "))
+for i in range(2, int(ipt5/2)+1):
+  tmp = ""
+  for j in range(ipt5, 0, -1):
+    if i*j < ipt5:
+      tmp += f"{i*j} "
+  print(tmp)
+
+i=int(input("Number: "))
+s=input("String: ")
+if(i != 0):
+  print(i if set("aeiou").intersection(s) else i if i>=42 else s)
+
+def cipher(string: str, rot: int) -> str:
+  t = ""
+  for c in string:
+    if(c.isalpha()):
+      b = ord('A' if c.isupper() else 'a')
+      t += chr((ord(c) - b + rot) % 26 + b)
+    else:
+      t += c
+  return t
+
+i1 = input("Cipher text: ")
+i2 = int(input("Cipher key: "))
+
+c = cipher(i1, i2)
+d = cipher(c, -i2)
+print(c)
+print(d)
